e8ea3ffb41fab52d326bf5addd9f4e2fa678e1c4
Ophir Bucai <ophirbucai@gmail.com>
2025-01-30 13:59:29 +0200

    utilize generic api response type

--- src/types/flight-response.type.ts
+++ src/types/flight-response.type.ts
@@ -1,53 +1,39 @@
+import type { APIResponse } from "./api-response.type";
+import type { Carrier } from "./carrier.type";
 import type { Itinerary } from "./itinerary.type";
 
-export type FlightResponse = {
-  status: boolean;
-  timestamp: number;
-  sessionId: string;
-  data: {
-    context: {
-      status: "incomplete" | string;
-      sessionId: string;
-      totalResults: number;
-    };
-    itineraries: Itinerary[];
-    messages: unknown[];
-    filterStats: {
-      duration: {
-        min: number;
-        max: number;
-        multiCityMin: number;
-        multiCityMax: number;
-      };
-      airports: {
-        city: string;
-        airports: {
-          id: string;
-          entityId: string;
-          name: string;
-        }[];
-      }[];
-      carriers: [
-        {
-          id: -32480;
-          alternateId: "BA";
-          logoUrl: "https://logos.skyscnr.com/images/airlines/favicon/BA.png";
-          name: "British Airways";
-        },
-        {
-          id: -30789;
-          alternateId: "PA";
-          logoUrl: "https://logos.skyscnr.com/images/airlines/favicon/PA.png";
-          name: "Fly Play";
-        },
-      ];
-      stopPrices: {
-        direct: { isPresent: boolean; formattedPrice: string };
-        one: { isPresent: boolean; formattedPrice: string };
-        twoOrMore: { isPresent: boolean };
-      };
-    };
-    flightsSessionId: string;
-    destinationImageUrl: string;
+export type FlightResult = {
+  context: {
+    status: "incomplete" | string;
+    sessionId: string;
+    totalResults: number;
   };
+  itineraries: Itinerary[];
+  messages: unknown[];
+  filterStats: {
+    duration: {
+      min: number;
+      max: number;
+      multiCityMin: number;
+      multiCityMax: number;
+    };
+    airports: {
+      city: string;
+      airports: {
+        id: string;
+        entityId: string;
+        name: string;
+      }[];
+    }[];
+    carriers: Carrier[];
+    stopPrices: {
+      direct: { isPresent: boolean; formattedPrice: string };
+      one: { isPresent: boolean; formattedPrice: string };
+      twoOrMore: { isPresent: boolean };
+    };
+  };
+  flightsSessionId: string;
+  destinationImageUrl: string;
 };
+
+export type FlightResponse = APIResponse<FlightResult[]>;
