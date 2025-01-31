1ea19cfad624fd5656b7645fba9819c114bcb40c
Ophir Bucai <ophirbucai@gmail.com>
2025-01-31 15:48:55 +0200

    validate dayjs object

--- src/schemas/search-form.schema.ts
+++ src/schemas/search-form.schema.ts
@@ -1,12 +1,12 @@
-import type { Dayjs } from "dayjs";
+import { type Dayjs, isDayjs } from "dayjs";
 import { z } from "zod";
 
 export const searchFormSchema = z
   .object({
     origin: z.object({ skyId: z.string(), entityId: z.string() }),
     destination: z.object({ skyId: z.string(), entityId: z.string() }),
-    date: z.custom<Dayjs>(),
-    returnDate: z.custom<Dayjs>().optional(),
+    date: z.custom<Dayjs>(isDayjs, "Invalid departure date"),
+    returnDate: z.custom<Dayjs>(isDayjs, "Invalid return date").optional(),
     passengers: z.object({
       adults: z.number().min(1),
     }),
