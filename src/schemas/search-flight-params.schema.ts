f715defa6ed7cd49e2a57210e786791a2be028de
Ophir Bucai <ophirbucai@gmail.com>
2025-02-02 09:43:29 +0200

    utilize nullish instead of optional in return date param

--- src/schemas/search-flight-params.schema.ts
+++ src/schemas/search-flight-params.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { CabinClass } from "../types";
 
-const formatDate = (date?: Date) => date?.toISOString().slice(0, 10);
+const formatDate = (date?: Date | null) => date?.toISOString().slice(0, 10);
 
 export const searchFlightsParamsSchema = z.object({
   originSkyId: z.string(),
@@ -9,7 +9,7 @@ export const searchFlightsParamsSchema = z.object({
   originEntityId: z.preprocess(Number, z.number().int()),
   destinationEntityId: z.preprocess(Number, z.number().int()),
   date: z.coerce.date().transform(formatDate),
-  returnDate: z.coerce.date().optional().transform(formatDate),
+  returnDate: z.coerce.date().nullish().transform(formatDate),
   cabinClass: z.nativeEnum(CabinClass).default(CabinClass.Economy),
   adults: z.number().int().min(1).default(1),
   children: z.number().int().optional(),
